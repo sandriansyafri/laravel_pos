e595cfbd4fef6325ec9f20d571c98b077fc17d86
sandriannsyafri <sandriannsyafri@gmail.com>
2021-12-31 22:18:41 +0700

    integration template ADMIN LTE

---
+++ resources/views/layouts/backend/parts/sidebar.blade.php
@@ -0,0 +1,118 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="index3.html" class="brand-link">
+        <img
+            src="dist/img/AdminLTELogo.png"
+            alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3"
+            style="opacity: 0.8"
+        />
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img
+                    src="dist/img/user2-160x160.jpg"
+                    class="img-circle elevation-2"
+                    alt="User Image"
+                />
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Alexander Pierce</a>
+            </div>
+        </div>
+
+        <nav class="mt-2">
+            <ul
+                class="nav nav-pills nav-sidebar flex-column"
+                data-widget="treeview"
+                role="menu"
+                data-accordion="false"
+            >
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-header">MASTER</li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-cube"></i>
+                        <p>Kategori</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-cubes"></i>
+                        <p>Produk</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-id-card"></i>
+                        <p>Member</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-truck"></i>
+                        <p>Supplier</p>
+                    </a>
+                </li>
+                <li class="nav-header">TRANSACTION</li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-money-check"></i>
+                        <p>Pembelian</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-wallet"></i>
+                        <p>Pengeluaran</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-dollar-sign"></i>
+                        <p>Penjualan</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-coins"></i>
+                        <p>Transaksi Lama</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-cash-register"></i>
+                        <p>Transaksi Baru</p>
+                    </a>
+                </li>
+                <li class="nav-header">REPORT</li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-file-alt"></i>
+                        <p>Laporan</p>
+                    </a>
+                </li>
+                <li class="nav-header">SETTINGS</li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-users"></i>
+                        <p>User</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">
+                        <i class="nav-icon fa fa-cogs"></i>
+                        <p>Pengaturan</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
